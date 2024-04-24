107b7cb99d57ece4b1c1afe62f12ec762d2ca6cb
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-04-24 20:56:04 +0600

    add liberary multer

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const router = express.Router();
+const multer = require('multer');
+
+const uploadDestination = 'uploads'
+
+//Показать, где хранить файлы
+const storage = multer.diskStorage({
+    destination: uploadDestination,
+    filename: function (req, file, callback) {
+        callback(null, file.originalname)
+    }
+});
+
+const uploads = multer({storage: storage})
+
 
 router.get('/register', (req, res) => {
     res.send('register');
