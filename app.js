bdf11ed9ce3c603d45a2ee132fe22fc3ebe4509f
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-04-24 20:38:50 +0600

    creating the first route

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-
-
 const app = express();
 
 app.set('view engine', 'jade');
@@ -14,6 +12,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/api', require('./routes'))
 
 
 // catch 404 and forward to error handler
