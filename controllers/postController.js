9250875223757a0c2ab05c59618e16e1ed1a3878
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-04-28 22:35:54 +0600

    add deletePost in postController

--- controllers/postController.js
+++ controllers/postController.js
@@ -94,7 +94,23 @@ const PostController = {
             return res.status(404).json({error: 'Пост не найден'})
         }
 
+        if (post.authorId !== req.user.userId) {
+            return res.status(403).json({error: 'Нет доступа'})
+        }
 
+        try {
+            const transaction = await prisma.$transaction([
+                prisma.comment.deleteMany({where: {postId: id}}),
+                prisma.like.deleteMany({where: {postId: id}}),
+                prisma.post.delete({where: {id}})
+            ])
+
+            res.json(transaction)
+        } catch (error) {
+            console.error('Delete post error', error)
+
+            res.status(500).json({error: 'Internal server error'})
+        }
     }
 };
 
