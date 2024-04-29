39b920d025ae21a67bcf0c05653e3b4779f2497d
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-04-29 21:08:08 +0600

    bug fix with adding like

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -2,16 +2,16 @@ const {prisma} = require('../prisma/prisma-client');
 
 const LikeController = {
     likePost: async (req, res) => {
-        const {posId} = req.body;
+        const {postId} = req.body;
         const userId = req.user.userId;
 
-        if (!posId) {
+        if (!postId) {
             return res.status(400).json({error: 'Все поля обязательны'})
         }
 
         try {
             const existingLike = await prisma.like.findFirst({
-                where: {posId, userId}
+                where: {postId, userId}
             })
 
             if (existingLike) {
@@ -21,6 +21,8 @@ const LikeController = {
             const like = await prisma.like.create({
                 data: {postId, userId}
             })
+
+            res.json(like);
         } catch (error) {
             console.error('Error like post', error)
             res.status(500).json({error: 'Не получилось поставить лайк'})
