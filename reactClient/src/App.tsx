9c732f98b2ed207b2211073fca010d1acf2cdc3d
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-05-03 21:11:25 +0600

    delete unnecessary files

--- reactClient/src/App.tsx
+++ reactClient/src/App.tsx
@@ -3,8 +3,8 @@ import logo from "./logo.svg";
 
 const App = () => {
   return (
-    <Button color="primary">
-        Button Title      
+    <Button color="primary"  >
+        Button       
     </Button>
       
   )
