8fb30761cde72665c0158725cf4696928cff4645
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-05-04 20:44:12 +0600

    add new builder in userApi

--- reactClient/src/app/services/userApi.ts
+++ reactClient/src/app/services/userApi.ts
@@ -1,29 +1,47 @@
+import { register } from "module";
 import { api } from "./api";
+import { current } from "@reduxjs/toolkit";
+import { User } from '../types'
 
 export const userAPi = api.injectEndpoints({
     endpoints: (builder) => ({
-        login: builder.mutation({
-            query: (user) => ({
+        login: builder.mutation<
+            { token: string },
+            { email: string; password: string }
+        >({
+            query: (userData) => ({
                 url: '/login',
                 method: 'POST',
-                body: user
-            }),
-            invalidatesTags: ['user']
+                body: userData
+            })
         }),
-        register: builder.mutation({
-            query: (user) => ({
+        register: builder.mutation<
+            { email: string; passoword: string; name: string },
+            { email: string; passoword: string; name: string }
+        >({
+            query: (userData) => ({
                 url: '/register',
                 method: 'POST',
-                body: user
-            }),
-            invalidatesTags: ['user']
+                body: userData
+            })
         }),
-        logout: builder.mutation({
+        current: builder.query<User, void>({
             query: () => ({
-                url: '/logout',
-                method: 'POST'
-            }),
-            invalidatesTags: ['user']
-        })
+                url: '/current',
+                method: 'GET'
+            })
+        }),
+        getUserById: builder.query<User, string>({
+            query: (id) => ({
+                url: `/users/${id}`,
+                method: 'GET'
+            })
+        }),
+        updateUser: builder.mutation<User, {userData:FormData, id:string}>({
+            query: (userData, id) => ({
+                url: `/users/${id}`,
+                method: 'PUT',
+                body: userData
+        
     })
 })
