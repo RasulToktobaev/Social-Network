abcf8098f8d71cc1dd420d4b52d2379234c3e17a
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-05-10 20:36:13 +0600

    add new component

---
+++ reactClient/src/components/input/index.tsx
@@ -0,0 +1,48 @@
+import { Input as NextInput } from '@nextui-org/react';
+import React from 'react'
+import { useController } from 'react-hook-form';
+
+type Props = {
+    name: string;
+    label: string;
+    placeholder: string;
+    type?: string;
+    control: Control<any>
+    required?: string;
+    endContent: JSX.Element
+}
+
+export const Input: React.FC<Props> = (
+    {
+        name,
+        label,
+        placeholder,
+        type,
+        control,
+        required = '',
+        endContent,
+    }
+) => {
+
+    const {
+        fieldState: { invalid },
+        formState: { errors }
+    } = useController({
+        name,
+        control,
+        rules: {
+            required
+        }
+    });
+    return (
+        <NextInput
+            id={name}
+            label={label}
+            placeholder={placeholder}
+            type={type}
+            value
+        />
+    )
+}
+
+
