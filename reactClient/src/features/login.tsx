acea0864f1ba2f50ff5d25a3bf86b388aa822f0d
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-05-10 20:48:50 +0600

    add props Login in login

--- reactClient/src/features/login.tsx
+++ reactClient/src/features/login.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
+
+type Login = {
+    email: string;
+    password: string;
+}
 
 type Props = {
     setSelected:(value: string) => void;
 }
 
-export const Login = () => {
+export const Login: React.FC<Props> = () => {
+
+    const {
+        handleSubmit,
+        control,
+        formState: {
+            errors
+        }
+    } = useForm<Login>({
+        mode:'onChange',
+        reValidateMode:'onBlur',
+        defaultValues:{
+            email:'',
+            password:''
+        }
+    });
   return (
-    <div>
-      Login
-    </div>
+    <div>Login</div>
   )
 }
