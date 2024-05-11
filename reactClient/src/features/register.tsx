561eb9e48c54515eebfc3275ed5c5db4dd1b5d37
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-05-11 20:40:46 +0600

    reinstalling docker containers and starting to implement registration fields

---
+++ reactClient/src/features/register.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Register = {
+    email: string;
+    password: string;
+    name: string;
+}
+
+export const Register = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
