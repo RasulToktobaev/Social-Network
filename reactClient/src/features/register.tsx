042fe8cc7dfc2e7a4fffee8a5f7f85a8887b0c51
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-05-11 20:49:56 +0600

    add register

--- reactClient/src/features/register.tsx
+++ reactClient/src/features/register.tsx
@@ -1,15 +1,78 @@
 import React from 'react'
 
+import { useForm } from 'react-hook-form';
+import { Input } from '../components/input';
+import { Button, Link } from '@nextui-org/react';
+
 type Register = {
     email: string;
     password: string;
     name: string;
 }
 
-export const Register = () => {
+type Props = {
+    setSelected: (value: string) => void;
+}
+
+export const Register:React.FC<Props> = ({
+    setSelected
+}) => {
+
+    const {
+        handleSubmit,
+        control,
+        formState: {
+            errors
+        }
+    } = useForm<Register>({
+        mode: 'onChange',
+        reValidateMode: 'onBlur',
+        defaultValues: {
+            email: '',
+            password: '',
+            name: ''
+        }
+    })
+
+    const onSubmit = async (data: Register) => {
+        try {
+            
+        } catch (err) {
+
+        }
+    }
   return (
-    <div>
-      
-    </div>
+    <form className='flex flex-col gap-4' onSubmit={handleSubmit(onSubmit)}>
+            <Input
+                control={control}
+                name='email'
+                label='Email'
+                type='email'
+                required='Обязательное поле'
+            />
+            <Input
+                control={control}
+                name='password'
+                label='Пароль'
+                type='password'
+                required='Обязательное поле'
+            />
+
+            <p className="text-center text-small">
+                Нет аккаунта?{" "}
+                <Link
+                    size='sm'
+                    className='cursor-pointer'
+                    onPress={() => setSelected("sign-up")}
+                >
+                    Зарегистрируйтесь
+                </Link>
+            </p>
+            <div className="flex gap-2 justify-end">
+                <Button fullWidth color='primary' type='submit' isLoading={isLoading}>
+                    Войти
+                </Button>
+            </div>
+        </form>
   )
 }
